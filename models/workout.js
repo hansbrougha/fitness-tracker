16f70443d6b13479607afb973ce5db3c750347ae
Andrew Hansbrough <hansbroughku@gmail.com>
2020-07-21 22:25:02 -0500

    Updated workout model

--- models/workout.js
+++ models/workout.js
@@ -1,35 +1,49 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now()
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true
-      },
-      name: {
-        type: String,
-        trim: true
-      },
-      duration: {
-        type: Number
-      },
-      weight: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now()
+    },
+    exercises: [
+      {
+        type: {
+          type: String
+        },
+        name: {
+          type: String
+        },
+        duration: {
+          type: Number
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
       }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
     }
-  ]
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);

b9f8ba0ada1ca0803aa37e63d93d0f2b513cd764
Andrew Hansbrough <hansbroughku@gmail.com>
2020-07-21 22:30:10 -0500

    Refactored html routes to be accurate

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,19 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "public", "index.html"));
-});
+// router.get("/", (req, res) => {
+//   // console.log("hit");
+//   res.sendFile(path.join(__dirname + "../public/index.html"));
+// });
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "public", "exercise.html"));
+  console.log("hit");
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname + "public", "stats.html"));
+  // console.log(res);
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
+
+module.exports = router;

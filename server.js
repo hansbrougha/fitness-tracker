8477b1e071f87fe52eefda55caa3374a4c3ecd92
Andrew Hansbrough <hansbroughku@gmail.com>
2020-07-21 22:23:41 -0500

    Updated Server

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
+  // useUnifiedTopology: true
 });
 
 app.use(apiRoutes);

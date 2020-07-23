7e85e1d3e126e92d3f8d3fdbc98ada5ebeb4a69c
Andrew Hansbrough <hansbroughku@gmail.com>
2020-07-23 14:34:19 -0500

    Refactoring for deployment.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 
-// router.get("/", (req, res) => {
-//   // console.log("hit");
-//   res.sendFile(path.join(__dirname + "../public/index.html"));
-// });
+router.get("/", (req, res) => {
+  // console.log("hit");
+  res.sendFile(path.join(__dirname + "../public/index.html"));
+});
 
 router.get("/exercise", (req, res) => {
   console.log("hit");
